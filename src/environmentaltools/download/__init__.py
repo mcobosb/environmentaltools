3b2901173b21ccd8ba86dd5808e9fa372da1dbfe
Pedro <potinar@ugr.es>
2026-04-27 10:05:34 +0200

    update init file with new copernicus functions

--- src/environmentaltools/download/__init__.py
+++ src/environmentaltools/download/__init__.py
@@ -177,12 +177,15 @@ from .open_street_images import (
 )
 
 # Marine Copernicus functions
-from .marine_copernicus import (
+from .copernicus_cds_era5 import (
     ERA5DataDownloadConfig,
     ERA5DataDownloader,
     ERA5DataProcessor,
     download_era5_data,
 )
+
+from .copernicus_marine_data import (
+descargar_copernicus)
 
 __all__ = [
     # CORDEX data (hybrid: intake-esgf + PyESGF fallback)
@@ -214,4 +217,5 @@ __all__ = [
     "ERA5DataDownloader",
     "ERA5DataProcessor",
     "download_era5_data",
+    "descargar_copernicus"
 ]
